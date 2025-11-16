3daf116d801366dd88637c64d820a22f72da069e
Nam Phu <namphu9a6@gmail.com>
2025-11-16 23:21:35 +0700

    Add project files.

---
+++ QLCHBanAo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QLCHBanAo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
