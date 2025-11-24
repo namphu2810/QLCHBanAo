045f3a5f03f32226606c8aa205b0a6208bb4c433
Mỹ Kim <kim_dpm225428@student.agu.edu.vn>
2025-11-24 21:39:22 +0700

    fix model KH

---
+++ QLCHBanAo/Models/KhachHang.cs
@@ -0,0 +1,13 @@
+﻿namespace QLCHBanAo.Models
+{
+    public class KhachHang
+    {
+        public int Id { get; set; }
+
+        public string TenKhachHang { get; set; }
+        public string DiaChi { get; set; }
+        public string SoDienThoai { get; set; }
+
+        public bool HoatDong { get; set; }
+    }
+}
